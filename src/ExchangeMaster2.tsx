f02441e7518203aaad07adaad609e022a155c8f7
Ivan <js314head@gmail.com>
2020-12-15 23:07:33 +0100

    Start of project

---
+++ src/ExchangeMaster2.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './ExchangeMaster2.scss';
+
+function ExchangeMaster() {
+  return <div className="ExchangeMaster">ExchangeMaster</div>;
+}
+
+export default ExchangeMaster;
